2db361016a8cdc580a8964df6d7b813b818e1bb1
guangjian-yang <42929753+guangjian-yang@users.noreply.github.com>
2021-10-16 22:51:40 +0800

    Add files via upload

---
+++ spring-cloud-alibaba-nacos-demo/src/test/java/com/sunshine/springcloudalibabanacosdemo/SpringCloudAlibabaNacosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshine.springcloudalibabanacosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaNacosDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
